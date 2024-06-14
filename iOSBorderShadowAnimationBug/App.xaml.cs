05f6d8a57ae615b885e6df5152a8cf8f1a2df851
jonmdev <43948450+jonmdev@users.noreply.github.com>
2024-06-14 18:38:01 -0400

    add second test project

--- iOSBorderShadowAnimationBug/App.xaml.cs
+++ iOSBorderShadowAnimationBug/App.xaml.cs
@@ -1,9 +1,73 @@
-﻿namespace iOSBorderShadowAnimationBug {
-    public partial class App : Application {
-        public App() {
-            InitializeComponent();
+﻿using Microsoft.Maui.Controls.Shapes;
 
-            MainPage = new AppShell();
+namespace iOSBorderShadowAnimationBug {
+    public partial class App : Application {
+        enum TestProjects {
+            testProject1, //==> DEMONSTRATES FLASH AT CORNERS OF LOWER SCREEN "HELLO" BORDER
+                          //AS BORDER IS INITIALLY DRAWN SQUARE (RATHER THAN ROUND) ON THE FIRST "CLICK ME" BUTTON CLICK
+
+            testProject2 //==> DEMONSTRATES FLASH OF PRIOR COLOR ON EACH UPDATE
+                         //AS EACH TIME BORDER IS REDRAWN THE COLORATION LAGS ONE FRAME
+        }
+        public App() {
+
+            //=================================
+            //SET WHICH TEST PROJECT HERE
+            //=================================
+            TestProjects testProject = TestProjects.testProject1;
+            //=================================
+
+            ContentPage mainPage = new();
+            mainPage.BackgroundColor = Colors.CornflowerBlue;
+
+            if (testProject == TestProjects.testProject1) {
+                InitializeComponent();
+                MainPage = new AppShell();
+            }
+            else {
+                MainPage = mainPage;
+            }
+            AbsoluteLayout abs = new();
+            mainPage.Content = abs;
+
+            Border border = new();
+            border.BackgroundColor = Colors.Purple;
+            border.StrokeShape = new RoundRectangle() { CornerRadius = 20 };
+            border.Shadow = new Shadow() { Brush = Colors.Black, Radius = 5, Offset = new Point(2, 5) };
+            abs.Add(border);
+
+            Label label = new();
+            label.HorizontalOptions = LayoutOptions.Center;
+            label.Text = "TEST LABEL";
+            label.TextColor = Colors.White;
+            label.FontSize = 30;
+            border.Content = label;
+
+            mainPage.SizeChanged += delegate {
+                double screenWidth = mainPage.Width;
+                double screenHeight = mainPage.Height;
+                border.WidthRequest = screenWidth * 0.8;
+                border.TranslationX = (screenWidth - border.WidthRequest) * 0.5;
+                border.TranslationY = screenHeight * 0.3;
+            };
+
+            List<Color> colorList = new() { Colors.Purple, Colors.OrangeRed, Colors.Turquoise, Colors.DarkCyan };
+            List<string> textList = new() { "Hello", "Goodbye" };
+            
+            //timer
+            var timer = Application.Current.Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromSeconds(1 / 2f); //run timer at 2 fps 
+            timer.IsRepeating = true;
+            timer.Tick += delegate {
+                border.IsVisible = !border.IsVisible;
+                if (border.IsVisible) {
+                    label.Text = textList[Random.Shared.Next(0, textList.Count)];
+                    Color newBg = colorList[Random.Shared.Next(0, colorList.Count)];
+                    while (newBg == border.BackgroundColor) { newBg = colorList[Random.Shared.Next(0, colorList.Count)]; }
+                    border.BackgroundColor = newBg;
+                }
+            };
+            timer.Start();
         }
     }
 }
--- iOSBorderShadowAnimationBug/NotAnimatedBorderHandler.cs
+++ iOSBorderShadowAnimationBug/NotAnimatedBorderHandler.cs
@@ -14,38 +14,15 @@ namespace iOSBorderShadowAnimationBug {
     public class NotAnimatedBorderHandler : BorderHandler {
 
 #if IOS
-        public override void PlatformArrange(Rect rect) {
-            
-            CATransaction.Begin();
-            CATransaction.DisableActions = true;
-            CATransaction.AnimationDuration = 0;
-            
-            //ISSUE:
-            base.PlatformArrange(rect); 
-
-            //this doesn't help our problem now as it calls BorderHandler.iOS.cs 's PlatformArrange which restarts the CATransaction
-            //technically need base.base.PlatformArrange(rect) but need reflection to get it as per:
-            //https://stackoverflow.com/questions/2323401/how-to-call-base-base-method
-            //https://stackoverflow.com/questions/1006530/how-to-call-a-second-level-base-class-method-like-base-base-gethashcode
-            
-            CATransaction.Commit();
-        }
-
-
         private class BorderContentView : ContentView {
             public override void LayoutSubviews() {
-                NSMutableDictionary dict = new();
-                dict.TryAdd(new NSString("sublayers"), new NSNull());
-                dict.TryAdd(new NSString("content"), new NSNull());
 
                 Layer.RemoveAllAnimations();
-                Layer.Actions = dict;
 
                 if (Layer.Sublayers != null) {
 
                     for (int i=0; i < Layer.Sublayers.Count(); i++) {
                         Layer.Sublayers[i].RemoveAllAnimations();
-                        Layer.Sublayers[i].Actions = dict; //  layer.actions = ["sublayers":NSNull()] 
                     }
                 }
                 
